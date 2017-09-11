30308381831a6199c4241cc92ea0ebe7a59df53d
anna0207 <annaoelgaardnielsen@hotmail.com>
2017-09-11 22:05:02 +0200

    Latitude and Longitude implemented

--- app/src/main/java/com/example/anna/testapp/MainActivity.java
+++ app/src/main/java/com/example/anna/testapp/MainActivity.java
@@ -1,15 +1,23 @@
 package com.example.anna.testapp;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.location.Location;
+import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -17,6 +25,16 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.view.animation.RotateAnimation;
 import android.view.animation.Animation;
+import android.Manifest;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.gms.vision.text.Text;
+
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
     private SensorManager sensorManager;
@@ -25,6 +43,10 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     private TextView compassAngle;
     private Sensor compass;
     private float currentDegree = 0f;
+    private FusedLocationProviderClient fusedLocationClient;
+    private String _tag = "DebugLog";
+
+    public final int COARSE_LOCATION_REQUEST = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,27 +54,87 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        //fab.setOnClickListener(new View.OnClickListener() {
-        //    @Override
-        //    public void onClick(View view) {
-        //        startAccActivity(view);
-                //Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                //        .setAction("Action", null).show();
-        //    }
-        //});
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            checkPermission();
+        } else {
+            updateLocationData();
+        }
         sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
         sensorManager.registerListener(this, sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_NORMAL);
-        //AccelerometerActivity accActivity = new AccelerometerActivity();
-        //accActivity.onCreate(savedInstanceState);
 
-        image = (ImageView)findViewById(R.id.imageViewCompass);
-        compassAngle = (TextView)findViewById(R.id.angle);
+        image = (ImageView) findViewById(R.id.imageViewCompass);
+        compassAngle = (TextView) findViewById(R.id.angle);
         compass = sensorManager.getDefaultSensor(Sensor.TYPE_ORIENTATION);
-        if(compass != null){
+        if (compass != null) {
             sensorManager.registerListener(this, compass, SensorManager.SENSOR_DELAY_NORMAL);
         }
+    }
+
+    public void checkPermission(){
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED ||
+                ContextCompat.checkSelfPermission(this,Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                ){//Can add more as per requirement
+
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION,Manifest.permission.ACCESS_COARSE_LOCATION},
+                    COARSE_LOCATION_REQUEST);
+        } else {
+            updateLocationData();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case COARSE_LOCATION_REQUEST: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    updateLocationData();
+
+                }
+                return;
+            }
+
+        }
+
+    }
+
+    private void updateLocationData() {
+        try {
+            Log.i(_tag, "1");
+            Task<Location> task = fusedLocationClient.getLastLocation();
+            Log.i(_tag, "1,5");
+            OnSuccessListener<Location> listener = new OnSuccessListener<Location>() {
+                @Override
+                public void onSuccess(Location location) {
+                    Log.i(_tag, "2");
+                    if (location != null) {
+                        Log.i(_tag, "3");
+                        TextView lat = (TextView)findViewById(R.id.latitude);
+                        TextView lon = (TextView)findViewById(R.id.longitude);
+                        TextView time = (TextView)findViewById(R.id.time);
+                        lat.setText(String.valueOf(location.getLatitude()));
+                        lon.setText(String.valueOf(location.getLongitude()));
+                        Date last = new Date(location.getTime());
+                        time.setText(last.toString());
+                    }
+                    Log.i(_tag, "4");
+                }
+            };
+            Log.i(_tag, "4,5");
+            task.addOnSuccessListener(this, listener);
+            Log.i(_tag, "5");
+        } catch (SecurityException ex) {
+            Log.i(_tag, ex.getMessage());
+        }
+    }
+
+    protected void createLocationRequest() {
+        LocationRequest mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(10000);
+        mLocationRequest.setFastestInterval(5000);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
     }
 
     @Override
@@ -64,13 +146,13 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     public void onSensorChanged(SensorEvent event) {
         if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
             ax = event.values[0];
-            TextView xaxis = (TextView)this.findViewById(R.id.xaxis);
+            TextView xaxis = (TextView) this.findViewById(R.id.xaxis);
             xaxis.setText(Math.round(ax * 1000.0) / 1000.0 + "");
             ay = event.values[1];
-            TextView yaxis = (TextView)this.findViewById(R.id.yaxis);
+            TextView yaxis = (TextView) this.findViewById(R.id.yaxis);
             yaxis.setText(Math.round(ay * 1000.0) / 1000.0 + "");
             az = event.values[2];
-            TextView zaxis = (TextView)this.findViewById(R.id.zaxis);
+            TextView zaxis = (TextView) this.findViewById(R.id.zaxis);
             zaxis.setText(Math.round(az * 1000.0) / 1000.0 + "");
         }
         if (event.sensor.getType() == Sensor.TYPE_ORIENTATION) {
@@ -95,10 +177,6 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         return true;
     }
 
-    //public void startAccActivity(View view) {
-    //    Intent intent = new Intent(this, AccelerometerActivity.class);
-    //    startActivity(intent);
-    //}
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
